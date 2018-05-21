16a627ec47474a003b2d940b63220b79db613d68
zenglong <552845376@qq.com>
2018-05-21 22:00:02 +0800

    解决合并更新冲突

--- nim_sdk_csharp/Src/Core/api/Device/NIMDeviceAPI.cs
+++ nim_sdk_csharp/Src/Core/api/Device/NIMDeviceAPI.cs
@@ -267,149 +267,140 @@ namespace NIM
         }
 
 #if NIMAPI_UNDER_WIN_DESKTOP_ONLY
-		/// <summary>
-		/// 启动辅助的摄像头，摄像头数据通过SetVideoCaptureDataCb设置采集回调返回，不直接通过视频通话发送给对方，并且不参与设备监听检测
-		/// </summary>
-		/// <param name="id">摄像头标识，用于开关及数据回调时的对应，不能为空。（同一id下设备将不重复启动，如果设备device_path不同会先关闭前一个设备开启新设备）</param>
-		/// <param name="device_path">设备路径</param>
-		/// <param name="fps">摄像头为采样频率</param>
-		/// <param name="json_extension">打开摄像头是允许设置 kNIMDeviceWidth 和 kNIMDeviceHeight，并取最接近设置值的画面模式</param>
-		/// <param name="handler">回调</param>
-		///<returns>无返回值</returns>
-		public static void StartExtendCamera(string id, string device_path, uint fps, string json_extension, StartDeviceResultHandler handler)
-		{
-			var ptr = NimUtility.DelegateConverter.ConvertToIntPtr(handler);
-			DeviceNativeMethods.nim_vchat_start_extend_camera(id, device_path, fps, json_extension, StartExtendCameraCb, ptr);
-		}
+        /// <summary>
+        /// 启动辅助的摄像头，摄像头数据通过SetVideoCaptureDataCb设置采集回调返回，不直接通过视频通话发送给对方，并且不参与设备监听检测
+        /// </summary>
+        /// <param name="id">摄像头标识，用于开关及数据回调时的对应，不能为空。（同一id下设备将不重复启动，如果设备device_path不同会先关闭前一个设备开启新设备）</param>
+        /// <param name="device_path">设备路径</param>
+        /// <param name="fps">摄像头为采样频率</param>
+        /// <param name="json_extension">打开摄像头是允许设置 kNIMDeviceWidth 和 kNIMDeviceHeight，并取最接近设置值的画面模式</param>
+        /// <param name="handler">回调</param>
+        ///<returns>无返回值</returns>
+        public static void StartExtendCamera(string id, string device_path, uint fps, string json_extension, StartDeviceResultHandler handler)
+        {
+            var ptr = NimUtility.DelegateConverter.ConvertToIntPtr(handler);
+            DeviceNativeMethods.nim_vchat_start_extend_camera(id, device_path, fps, json_extension, StartExtendCameraCb, ptr);
+        }
 
-		/// <summary>
-		/// 结束辅助摄像头
-		/// </summary>
-		/// <param name="id">摄像头标识id，如果为空，则关闭所有辅助摄像头</param>
-		/// <returns>无返回值</returns>
-		public static void StopExtendCamera(string id)
-		{
-			DeviceNativeMethods.nim_vchat_stop_extend_camera(id, "");
-		}
+        /// <summary>
+        /// 结束辅助摄像头
+        /// </summary>
+        /// <param name="id">摄像头标识id，如果为空，则关闭所有辅助摄像头</param>
+        /// <returns>无返回值</returns>
+        public static void StopExtendCamera(string id)
+        {
+            DeviceNativeMethods.nim_vchat_stop_extend_camera(id, "");
+        }
 
-		/// <summary>
-		/// 添加设备监听（摄像头和麦克风） 注意监听设备后底层会定时检查设备情况，在不需要监听后请移除
-		/// </summary>
-		/// <param name="type">设备类型（kNIMDeviceTypeAudioIn和kNIMDeviceTypeVideo有效）</param>
-		/// <param name="handler">回调</param>
-		/// <returns>无返回值</returns>
-		public static void AddDeviceStatusCb(NIMDeviceType type, DeviceStatusHandler handler)
-		{
-			var ptr = NimUtility.DelegateConverter.ConvertToIntPtr(handler);
-			DeviceNativeMethods.nim_vchat_add_device_status_cb(type, DeviceStatusCb, ptr);
-		}
+        /// <summary>
+        /// 添加设备监听（摄像头和麦克风） 注意监听设备后底层会定时检查设备情况，在不需要监听后请移除
+        /// </summary>
+        /// <param name="type">设备类型（kNIMDeviceTypeAudioIn和kNIMDeviceTypeVideo有效）</param>
+        /// <param name="handler">回调</param>
+        /// <returns>无返回值</returns>
+        public static void AddDeviceStatusCb(NIMDeviceType type, DeviceStatusHandler handler)
+        {
+            var ptr = NimUtility.DelegateConverter.ConvertToIntPtr(handler);
+            DeviceNativeMethods.nim_vchat_add_device_status_cb(type, DeviceStatusCb, ptr);
+        }
 
-		/// <summary>
-		/// 移除设备监听（摄像头和麦克风）
-		/// </summary>
-		/// <param name="type">设备类型</param>
-		/// <returns>无返回值</returns>
-		public static void RemoveDeviceStatusCb(NIMDeviceType type)
-		{
-			DeviceNativeMethods.nim_vchat_remove_device_status_cb(type);
-		}
+        /// <summary>
+        /// 移除设备监听（摄像头和麦克风）
+        /// </summary>
+        /// <param name="type">设备类型</param>
+        /// <returns>无返回值</returns>
+        public static void RemoveDeviceStatusCb(NIMDeviceType type)
+        {
+            DeviceNativeMethods.nim_vchat_remove_device_status_cb(type);
+        }
 
-		/// <summary>
-		/// 监听采集的视频数据
-		/// </summary>
-		/// <param name="handler">回调</param>
-        /// <param name="videoJsonEx">json 参数封装类</param>
-		/// <returns>无返回值</returns>
-		public static void SetVideoCaptureDataCb(VideoDataHandler handler, NIMVChatCustomVideoJsonEx videoJsonEx)
-		{
-			string json_extension = "";
-			if(videoJsonEx==null)
-			{
-				videoJsonEx = new NIMVChatCustomVideoJsonEx();
-				videoJsonEx.VideoSubType = Convert.ToInt32(NIMVideoSubType.kNIMVideoSubTypeARGB);
-			}
-			json_extension = videoJsonEx.Serialize();
-			var ptr = NimUtility.DelegateConverter.ConvertToIntPtr(handler);
-			DeviceNativeMethods.nim_vchat_set_video_data_cb(true, json_extension, VideoDataCb, ptr);
-		}
-
-		/// <summary>
-		/// 监听接收的视频数据
-		/// </summary>
-		/// <param name="handler">回调</param>
-        /// <param name="videoJsonEx">json参数封装类</param>
-		/// <returns>无返回值</returns>
-		public static void SetVideoReceiveDataCb(VideoDataHandler handler, NIMVChatCustomVideoJsonEx videoJsonEx)
-		{
+        /// <summary>
+        /// 监听采集的视频数据
+        /// </summary>
+        /// <param name="handler">回调</param>
+        /// <returns>无返回值</returns>
+        public static void SetVideoCaptureDataCb(VideoDataHandler handler, NIMVChatCustomVideoJsonEx videoJsonEx)
+        {
             string json_extension = "";
-            if (videoJsonEx == null)
+            if(videoJsonEx==null)
             {
                 videoJsonEx = new NIMVChatCustomVideoJsonEx();
                 videoJsonEx.VideoSubType = Convert.ToInt32(NIMVideoSubType.kNIMVideoSubTypeARGB);
             }
             json_extension = videoJsonEx.Serialize();
             var ptr = NimUtility.DelegateConverter.ConvertToIntPtr(handler);
-			DeviceNativeMethods.nim_vchat_set_video_data_cb(false, json_extension, VideoDataCb, ptr);
-		}
+            DeviceNativeMethods.nim_vchat_set_video_data_cb(true, json_extension, VideoDataCb, ptr);
+        }
 
-		/// <summary>
-		/// 音频采集音量，默认255
-		/// </summary>
-		public static byte AudioCaptureVolumn
-		{
-			get { return DeviceNativeMethods.nim_vchat_get_audio_volumn(true); }
-			set { DeviceNativeMethods.nim_vchat_set_audio_volumn(value, true); }
-		}
+        /// <summary>
+        /// 监听接收的视频数据
+        /// </summary>
+        /// <param name="handler">回调</param>
+        /// <returns>无返回值</returns>
+        public static void SetVideoReceiveDataCb(VideoDataHandler handler, string json_extension = "")
+        {
+            var ptr = NimUtility.DelegateConverter.ConvertToIntPtr(handler);
+            DeviceNativeMethods.nim_vchat_set_video_data_cb(false, json_extension, VideoDataCb, ptr);
+        }
 
-		/// <summary>
-		/// 音频播放音量，默认255
-		/// </summary>
-		public static byte AudioPlayVolumn
-		{
-			get { return DeviceNativeMethods.nim_vchat_get_audio_volumn(false); }
-			set { DeviceNativeMethods.nim_vchat_set_audio_volumn(value, false); }
-		}
+        /// <summary>
+        /// 音频采集音量，默认255
+        /// </summary>
+        public static byte AudioCaptureVolumn
+        {
+            get { return DeviceNativeMethods.nim_vchat_get_audio_volumn(true); }
+            set { DeviceNativeMethods.nim_vchat_set_audio_volumn(value, true); }
+        }
 
-		/// <summary>
-		/// 设置麦克风音量自动调节, 默认不自动调节
-		/// </summary>
-		public static bool AudioCaptureAutoVolumn
-		{
-			get { return DeviceNativeMethods.nim_vchat_get_audio_input_auto_volumn(); }
-			set { DeviceNativeMethods.nim_vchat_set_audio_input_auto_volumn(value); }
-		}
+        /// <summary>
+        /// 音频播放音量，默认255
+        /// </summary>
+        public static byte AudioPlayVolumn
+        {
+            get { return DeviceNativeMethods.nim_vchat_get_audio_volumn(false); }
+            set { DeviceNativeMethods.nim_vchat_set_audio_volumn(value, false); }
+        }
+
+        /// <summary>
+        /// 设置麦克风音量自动调节, 默认不自动调节
+        /// </summary>
+        public static bool AudioCaptureAutoVolumn
+        {
+            get { return DeviceNativeMethods.nim_vchat_get_audio_input_auto_volumn(); }
+            set { DeviceNativeMethods.nim_vchat_set_audio_input_auto_volumn(value); }
+        }
 
 
 
-				/// <summary>
-		/// 自定义视频数据接口
-		/// </summary>
-		/// <param name="time">时间毫秒级</param>
-		/// <param name="data">视频数据yuv420格式</param>
-		/// <param name="size">data的数据长度 sizeof(char)</param>
-		/// <param name="width">画面宽度</param>
-		/// <param name="height">画面高度</param>
-		/// <param name="info">扩展Json封装类</param>
-		/// <returns> bool true 调用成功，false 调用失败</returns>
-		public static bool CustomVideoData(ulong time, IntPtr data, uint size, uint width, uint height, NIMCustomVideoDataInfo info)
-		{
-			string jsonExtension = "";
-			if (info != null)
-				jsonExtension = info.Serialize();
-			return DeviceNativeMethods.nim_vchat_custom_video_data(time, data, size, width, height, jsonExtension);
-		}
+                /// <summary>
+        /// 自定义视频数据接口
+        /// </summary>
+        /// <param name="time">时间毫秒级</param>
+        /// <param name="data">视频数据yuv420格式</param>
+        /// <param name="size">data的数据长度 sizeof(char)</param>
+        /// <param name="width">画面宽度</param>
+        /// <param name="height">画面高度</param>
+        /// <param name="info">扩展Json封装类</param>
+        /// <returns> bool true 调用成功，false 调用失败</returns>
+        public static bool CustomVideoData(ulong time, IntPtr data, uint size, uint width, uint height, NIMCustomVideoDataInfo info)
+        {
+            string jsonExtension = "";
+            if (info != null)
+                jsonExtension = info.Serialize();
+            return DeviceNativeMethods.nim_vchat_custom_video_data(time, data, size, width, height, jsonExtension);
+        }
 
-		/// <summary>
-		/// 设置底层针对麦克风采集数据处理开关接口，默认全开（此接口是全局接口，在sdk初始化后设置一直有效）
-		/// </summary>
-		/// <param name="aec">true 标识打开回音消除功能，false 标识关闭</param>
-		/// <param name="ns">true 标识打开降噪功能，false 标识关闭</param>
-		/// <param name="vid">true 标识打开人言检测功能，false 标识关闭</param>
-		/// <returns>无返回值</returns>
-		public static void SetAudioProcessInfo(bool aec, bool ns, bool vid)
-		{
-			DeviceNativeMethods.nim_vchat_set_audio_process_info(aec, ns, vid);
-		}
+        /// <summary>
+        /// 设置底层针对麦克风采集数据处理开关接口，默认全开（此接口是全局接口，在sdk初始化后设置一直有效）
+        /// </summary>
+        /// <param name="aec">true 标识打开回音消除功能，false 标识关闭</param>
+        /// <param name="ns">true 标识打开降噪功能，false 标识关闭</param>
+        /// <param name="vid">true 标识打开人言检测功能，false 标识关闭</param>
+        /// <returns>无返回值</returns>
+        public static void SetAudioProcessInfo(bool aec, bool ns, bool vid)
+        {
+            DeviceNativeMethods.nim_vchat_set_audio_process_info(aec, ns, vid);
+        }
 
         /// <summary>
         ///  监听音频数据（可以不监听，通过启动设备kNIMDeviceTypeAudioOut和kNIMDeviceTypeAudioOutChat由底层播放）
@@ -431,22 +422,6 @@ namespace NIM
         public static void SetAudioHowlingSuppression(bool work)
         {
             DeviceNativeMethods.nim_vchat_set_audio_howling_suppression(work);
-        }
-
-        /// <summary>
-        /// 自定义音频伴音数据接口，不需要打开自定义数据开关, 采样时间必须为10ms的整数倍, 采样位深只支持16
-        /// </summary>
-        /// <param name="id">伴音数据id</param>
-        /// <param name="time">time 时间毫秒级</param>
-        /// <param name="data">音频数据pcm格式</param>
-        /// <param name="size">data的数据长度</param>
-        /// <param name="rate">音频采样频</param>
-        /// <param name="channels">声道数</param>
-        /// <param name="json_extension">json_extension 扩展</param>
-        /// <returns>bool true 调用成功，false 调用失败</returns>
-        public static bool NimVchatAccompanyingSound(Byte id, UInt64 time, IntPtr data, UInt32 size, UInt32 rate, UInt32 channels,string json_extension)
-        {
-            return DeviceNativeMethods.nim_vchat_accompanying_sound(id, time, data, size, rate, channels, json_extension);
         }
 #endif
 
